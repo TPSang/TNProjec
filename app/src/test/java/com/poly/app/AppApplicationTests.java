20ab9529aac37f25383e62a880ecc1f646007996
sangtp <tangphuocsang7698@gmail.com>
2023-08-05 08:56:40 +0700

    ProJect

---
+++ app/src/test/java/com/poly/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
